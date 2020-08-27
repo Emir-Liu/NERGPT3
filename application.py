6c317a443683cf6dc932ca983304dda53d1af551
Ricky Ma <mr.rickyma@gmail.com>
2020-08-26 17:24:22 -0700

    Added functionality for CSV/excel uploads

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 import openai
 import ast
 import os
+import base64
+import io
 import pandas as pd
 from dotenv import load_dotenv
 from pathlib2 import Path
@@ -22,6 +24,10 @@ app.css.config.serve_locally = True
 app.config["suppress_callback_exceptions"] = True
 
 ner_table = pd.DataFrame(columns=['values', 'tags'])
+upload_text = pd.DataFrame()
+upload_tab = True
+upload_column = ''
+textarea_string = ''
 primer = '''[]:Jim bought 300 shares of Acme Corp. in 2006.
 [("Jim", "person"), ("Acme Corp.", "organization"), ("2006", "year")]
 
@@ -99,10 +105,13 @@ def get_response(input_text, temp):
     return pairs
 
 
-def get_ner(text, temp, labels):
+def get_ner(temp, labels):
     # split text by sentences to process long text w/o errors
-    text = text.replace('“', '').replace('”', '').replace('"', '')
-    sentences = text.split(". ")
+    if upload_tab:
+        sentences = upload_text[upload_column]
+    else:
+        text = textarea_string.replace('“', '').replace('”', '').replace('"', '')
+        sentences = text.split(". ")
     pairs = []
     for sentence in sentences:
         input_text = '[' + labels + ']:' + sentence + '.'
@@ -117,6 +126,40 @@ def get_ner(text, temp, labels):
     return df
 
 
+def parse_contents(contents, filename):
+    global upload_text
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            upload_text = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            upload_text = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file. Please upload a CSV or Excel file.'
+        ])
+
+    return html.Div([
+        html.H5(filename),
+        dcc.Dropdown(
+            id='column-dropdown',
+            options=[{"label": i, "value": i} for i in list(upload_text)],
+            placeholder="Please select a column for tagging"
+        ),
+        html.Div(
+            dash_table.DataTable(
+                data=upload_text.to_dict('records'),
+                columns=[{'name': i, 'id': i} for i in upload_text.columns],
+                page_action='none',
+                style_table={'height': '300px', 'overflowY': 'auto', 'overflowX': 'auto'},
+                css=[{'selector': '.row', 'rule': 'margin: 0'}]
+            )
+        ),
+    ])
+
+
 def display_output_tab():
     content = dbc.Card(
         dbc.CardBody(
@@ -129,11 +172,13 @@ def display_output_tab():
                     editable=True,
                     export_format='csv',
                     export_headers="display",
-                    row_deletable=True
+                    row_deletable=True,
+                    css=[{'selector': '.row', 'rule': 'margin: 0'}],
+                    style_table={'height': '600px', 'overflowY': 'auto', 'overflowX': 'auto'},
                 )
             ]
         ),
-        className="mt-3",
+        className="card border-primary mb-3",
     )
     return content
 
@@ -147,7 +192,53 @@ def display_analysis_tab():
                 dcc.Graph(figure=fig),
             ]
         ),
-        className="mt-3",
+        className="card border-primary mb-3",
+    )
+    return content
+
+
+def display_input_tab():
+    content = dbc.Card(
+        dbc.CardBody(
+            [
+                dbc.Textarea(
+                    id='textarea',
+                    placeholder='Input your text for NER',
+                    style={'width': '100%', 'height': 300},
+                ),
+            ]
+        ),
+        className="card border-primary mb-3",
+    )
+    return content
+
+
+def display_upload_tab():
+    content = dbc.Card(
+        dbc.CardBody(
+            [
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select File')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    multiple=False
+                ),
+                html.Div(id='output-data-upload'),
+            ]
+        ),
+        className="card border-primary mb-3",
     )
     return content
 
@@ -155,20 +246,20 @@ def display_analysis_tab():
 @app.callback(
     Output("extract-output", "children"),
     [Input("extract-btn", "n_clicks")],
-    [State("textarea", "value"), State("temp-slider", "value"), State("label-input", "value")]
+    [State("temp-slider", "value"), State("label-input", "value")]
 )
-def extract_button(n_clicks, text, temp, labels):
+def extract_button(n_clicks, temp, labels):
     global ner_table
     if n_clicks is None:
-        return "Not clicked."
+        return "Please input text or upload file to begin."
     else:
         print(n_clicks)
-        print(text)
         print(temp)
+        print(labels)
         try:
             if labels is None:
                 labels = ''
-            ner_table = get_ner(text, temp, labels)
+            ner_table = get_ner(temp, labels)
             if labels != '':
                 labels = labels.replace(', ', ',').split(',')
                 ner_table = ner_table[ner_table['tags'].isin(labels)]
@@ -178,10 +269,10 @@ def extract_button(n_clicks, text, temp, labels):
                         dbc.Tab(label="Output", tab_id='tab-1'),
                         dbc.Tab(label="Analysis", tab_id='tab-2'),
                     ],
-                    id='tabs',
+                    id='output-tabs',
                     active_tab="tab-1"
                 ),
-                html.Div(id="content"),
+                html.Div(id="output-content"),
             ]
         except Exception as e:
             print(e)
@@ -195,13 +286,50 @@ def update_slider(value):
     return html.H3('Temperature: {}'.format(value))
 
 
-@app.callback(Output("content", "children"), [Input("tabs", "active_tab")])
-def switch_tab(tab_id):
+@app.callback(
+    Output('textarea-output', 'children'),
+    [Input('textarea', 'value')])
+def update_textarea(value):
+    global textarea_string
+    textarea_string = value
+    print(textarea_string)
+
+
+@app.callback(
+    Output('dropdown-output', 'children'),
+    [Input('column-dropdown', 'value')])
+def update_dropdown(value):
+    global upload_column
+    upload_column = value
+
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename')])
+def upload_data_output(content, filename):
+    if content is not None:
+        children = parse_contents(content, filename)
+        return children
+
+
+@app.callback(Output("input-content", "children"), [Input("input-tabs", "active_tab")])
+def switch_input_tab(tab_id):
+    global upload_tab
+    print(tab_id)
+    if tab_id == "tab-0":
+        upload_tab = False
+        return display_input_tab()
+    elif tab_id == "tab-1":
+        upload_tab = True
+        return display_upload_tab()
+
+
+@app.callback(Output("output-content", "children"), [Input("output-tabs", "active_tab")])
+def switch_output_tab(tab_id):
     if tab_id == "tab-1":
         return display_output_tab()
     elif tab_id == "tab-2":
         return display_analysis_tab()
-    return html.P("This shouldn't ever be displayed...")
 
 
 @app.callback(
@@ -240,13 +368,6 @@ app.layout = html.Div(
                               placeholder="Separate w/ commas, leave blank for automatic label generation",
                               type="text"),
                     html.Br(),
-                    html.H3("Text"),
-                    dbc.Textarea(
-                        id='textarea',
-                        placeholder='Input your text for NER',
-                        style={'width': '100%', 'height': 300},
-                    ),
-                    html.Br(),
                     html.Div(id='temp-output'),
                     dcc.Slider(
                         id='temp-slider',
@@ -255,6 +376,17 @@ app.layout = html.Div(
                         step=0.05,
                         value=0.5,
                     ),
+                    html.H3("Text"),
+                    dbc.Tabs(
+                        [
+                            dbc.Tab(label="Input", id="tab-0"),
+                            dbc.Tab(label="Upload", id="tab-1"),
+                        ],
+                        id="input-tabs",
+                        active_tab="tab-0"
+                    ),
+                    html.Div(id='input-content'),
+                    html.Br(),
                     dbc.Button("EXTRACT", id="extract-btn", block=True, color="primary", className="mr-1"),
                     html.Br(),
                     html.Img(
@@ -266,13 +398,18 @@ app.layout = html.Div(
                 width=6
             ),
             dbc.Col(
-                dbc.Spinner(
-                    html.Div(id='extract-output'),
-                    spinner_style={"width": "3rem", "height": "3rem"}
-                ),
+                [
+                    html.Br(),
+                    dbc.Spinner(
+                        html.Div(id='extract-output'),
+                        spinner_style={"width": "3rem", "height": "3rem"}
+                    )
+                ],
                 width=6
             )
         ]),
+        html.Div(id="dropdown-output", style={"display": "none"}),
+        html.Div(id="textarea-output", style={"display": "none"}),
     ],
     style={"margin-left": "5%", "margin-right": "5%", "margin-top": "5%", "margin-bottom": "5%"}
 )
